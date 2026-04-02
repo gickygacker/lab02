99ccb0d567c6b255f965c0815e4a0cf18307ea7b
Gicky Gacker <162728298+gickygacker@users.noreply.github.com>
2026-04-02 20:23:33 +0300

    add name into hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
-  cout << "Hello world" << endl;
-  return 0;
+int main() {
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello world from " << name << endl;
+    return 0;
 }
