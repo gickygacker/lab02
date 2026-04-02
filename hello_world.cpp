cb34d9a132c22bad57ceec4b1026c322ff1cb7d0
Gicky Gacker <162728298+gickygacker@users.noreply.github.com>
2026-04-02 20:55:21 +0300

    add comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
+//main function
 int main() {
     string name;
     cout << "Enter your name: ";
+    //user enters his name
     cin >> name;
     cout << "Hello world from " << name << endl;
     return 0;
