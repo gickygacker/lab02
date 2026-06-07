a4a84d1d021ab84f7f28417f4cafc6c7dfd39139
Gicky Gacker <govnillo.muchacho911@gmail.com>
2026-06-08 00:46:54 +0300

    Translate comment

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,7 @@
 //main function
 int main() {
     string name;
-    cout << "Enter your name: ";
+    cout << "Enter your имя: ";
     //user enters his name
     cin >> name;
     cout << "Hello world from " << name << endl;
